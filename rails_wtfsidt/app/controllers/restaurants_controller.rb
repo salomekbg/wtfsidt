798c697aa56af686056e516dca2bf8df7dea4a1e
JeffKatzy <JeffreyEricKatz@gmail.com>
2016-12-12 16:58:23 -0500

    Update restaurants_controller.rb

--- rails_wtfsidt/app/controllers/restaurants_controller.rb
+++ rails_wtfsidt/app/controllers/restaurants_controller.rb
@@ -5,6 +5,7 @@ class RestaurantsController < ApplicationController
     results = YelpApi.search(params["data"]["zipcode"])
     favorited_results = results.select do |result|
       result.favorites.where(user_id: find_user_id).length == 1
+      # instead of .length == 1 do .present?
     end
     favorite_ids = favorited_results.map do |result|
       result.id
@@ -14,8 +15,10 @@ class RestaurantsController < ApplicationController
 
   private
 
+
   def find_user_id
     Auth.decode(request.env["HTTP_AUTHORIZATION"])[0]["user_id"]
   end
+#   This should be a method called current_user which is in the application_controller.
 
 end
